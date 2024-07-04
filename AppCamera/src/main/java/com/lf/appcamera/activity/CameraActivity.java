9f6250d0c6a09600383b68669aadfa2b4a3274d8
shancheli <924906515@qq.com>
2024-07-04 11:57:27 +0800

    相机状态栏透明

--- AppCamera/src/main/java/com/lf/appcamera/activity/CameraActivity.java
+++ AppCamera/src/main/java/com/lf/appcamera/activity/CameraActivity.java
@@ -30,34 +30,41 @@ public class CameraActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        /* 在super.onCreate()之前添加代码块 start */
+        /* 隐藏标题栏 */
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        /* 隐藏状态栏 */
+        supportRequestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+        getWindow().setAttributes(lp);
+        /* 在super.onCreate()之前添加代码块 end */
+
         super.onCreate(savedInstanceState);
-
-        // 隐藏标题栏
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-
-        // 隐藏状态栏
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
         setContentView(R.layout.activity_camera);
 
-        jCameraView = findViewById(R.id.jcameraview);
         init();
     }
 
+    /* 添加函数onWindowFocusChanged */
     @Override
-    protected void onStart() {
-        super.onStart();
-
-        // 使用沉浸式模式
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     @Override
@@ -73,6 +80,7 @@ public class CameraActivity extends AppCompatActivity {
     }
 
     private void init() {
+        jCameraView = findViewById(R.id.jcameraview);
 
         //设置视频保存路径
         jCameraView.setSaveVideoPath(getCacheDir() + File.separator + "matisse");
@@ -99,7 +107,7 @@ public class CameraActivity extends AppCompatActivity {
             @Override
             public void captureSuccess(Bitmap bitmap) {
                 //获取图片bitmap
-                String path = FileUtil.saveBitmap("matisse", bitmap);
+                String path = FileUtil.saveBitmap("LFCamera", bitmap);
                 Intent intent = new Intent();
                 intent.putExtra(MatisseConst.EXTRA_RESULT_CAPTURE_IMAGE_PATH, path);
                 setResult(RESULT_OK, intent);
@@ -109,7 +117,7 @@ public class CameraActivity extends AppCompatActivity {
             @Override
             public void recordSuccess(String url, Bitmap firstFrame) {
                 //获取视频路径
-                String path = FileUtil.saveBitmap("matisse", firstFrame);
+                String path = FileUtil.saveBitmap("LFCamera", firstFrame);
                 Intent intent = new Intent();
                 intent.putExtra(MatisseConst.EXTRA_RESULT_CAPTURE_IMAGE_PATH, path);
                 intent.putExtra(MatisseConst.EXTRA_RESULT_CAPTURE_VIDEO_PATH, url);
--- app/src/main/java/com/lf/camera/MainActivity.java
+++ app/src/main/java/com/lf/camera/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lf.camera;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,17 +17,43 @@ import com.lf.appcamera.CaptureMode;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvPath;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
+        tvPath = findViewById(R.id.tv_path);
         findViewById(R.id.tv_toast).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AppCamera.from(MainActivity.this).setMode(CaptureMode.All).forResult(10001);
             }
         });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 10001 && resultCode == RESULT_OK) {
+            String capturePath = null;
+            String videoPath = null;
+
+            if((videoPath = AppCamera.obtainCaptureVideoResult(data))!=null){
+                //录制的视频
+                capturePath = AppCamera.obtainCaptureImageResult(data);
+                tvPath.setText("视频路径："+videoPath
+                        +" \n 第一帧图片："+capturePath);
+                Log.d("111---", videoPath);
+
+            }else if((capturePath = AppCamera.obtainCaptureImageResult(data))!=null){
+                tvPath.setText("拍照路径："+capturePath);
+                Log.d("111---", capturePath);
+            }
+        }
     }
 }
